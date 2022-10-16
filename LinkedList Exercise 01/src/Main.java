73b156b8a70c21bfe8be801bbe3e7c68dc0b3cf1
Daniel Baykov <danibaikov@abv.bg>
2022-10-16 13:28:58 +0300

    Adding 01

---
+++ LinkedList Exercise 01/src/Main.java
@@ -0,0 +1,14 @@
+import java.util.LinkedList;
+
+public class Main {
+    public static void main(String[] args) {
+
+        LinkedList<String> colors = new LinkedList<>();
+        colors.add("red");
+        colors.add("green");
+        colors.add("blue");
+        colors.add("yellow");
+
+        System.out.println(colors);
+    }
+}
